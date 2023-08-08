740d84b1fb2f62e52a355c68c13364bba6631f03
MohammedNayeemE <127304665+MohammedNayeemE@users.noreply.github.com>
2023-08-08 18:59:39 +0530

    Add files via upload

---
+++ client/src/image.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+function Image(){
+    return(
+        <>
+        <div className='image'>
+
+            <img src='https://marycjohansen.files.wordpress.com/2015/12/image.jpeg' alt='image'/>
+            <div className='caption'>
+                <p>Beauty Of Nature :) !! </p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Image;
