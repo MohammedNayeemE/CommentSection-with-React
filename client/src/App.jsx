933a443dc71b30cf2510212a49bc6802c36cdb82
MohammedNayeemE <127304665+MohammedNayeemE@users.noreply.github.com>
2023-09-11 18:36:38 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react"
-import { useEffect } from "react"
-import axios from "axios"
+import Header from "./Header"
+import Comments from "./Comments";
 import Image from './image'
 
 
@@ -16,108 +15,9 @@ function App(){
   )
 
 }
-function Header(){
-  return(
-    <>
-    <h1>Comments</h1>
-    </>
-  )
-}
-
-function Comments(){
-  const [list,setList] = useState([])
-  useEffect(()=>{
-    const fetchComments = async ()=>{
-
-      try{
-      const Comments = await axios.get('http://localhost:5000/');
-      const abc = Comments.data;
-      console.log(abc);
-      setList([...abc]);
-      }
-      catch(err){
-        console.log(err);
-      }
-
-    };
-
-    fetchComments();
-  } , []);
-  
-const addComment = async (name , comment) =>{
-  try{
-  const response = await axios.post('http://localhost:5000/' , {name , comment}) 
-  console.log(response.data);
- 
-  setList([...list , response.data])
-  }
-  catch(err){
-     console.log(err);
-  }
-
-}
 
 
-return(
-  <>
-  <div>
-  {list.map((item, index) => (
-        <div key={index}>
-          <p>Name: {item.name}</p>
-          <p>Comment: {item.comment}</p>
-        </div>
-      ))}
-    
-  </div>
 
-   
-  <AddComment onaddComment = {addComment}/>
-  
-  </>
-)
-
-
-}
-function AddComment({onaddComment}){
-  
-  const [name , setName] = useState("");
-  const [comment , setcomment] = useState("");
-
-  const handleClick = ()=>{
-    if(name && comment){
-      onaddComment(name ,  comment);
-      setName("");
-      setcomment("");
-    }
-  }
-
-  return(
-    <>
-    <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} className="input text"/>
-    <textarea placeholder="Add your Comment" value={comment} onChange={e => setcomment(e.target.value)} className="input text"/>
-    <button className="btn" onClick={handleClick}>
-      Add
-    </button>
-
-    </>
-    )
-    
-
-    
-  
-}
 
 export default App;
-
-
-/*
-{
-    name:"RM",
-    comment:"Hello"
-  },
-  {
-    name:"Jin",
-    comment:"Hey Stob It"
-  }
-  */
 
